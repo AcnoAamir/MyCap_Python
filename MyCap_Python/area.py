2d8dc3d68eba72ca5b1d5a01c592ddea71daed47
AcnoAamir <54440357+AcnoAamir@users.noreply.github.com>
2021-08-03 21:22:31 +0530

    Area program
    Assignment 1, are of circle

---
+++ MyCap_Python/area.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Acno-Aamir
+"""
+
+r=float(input("Enter the radius of the circle to find area for "))
+# pi=3.14159265358979323846
+# area=pi*r*r
+print("The area of circle with radius ",r," is ",(3.14159265358979323846*r*r)," sq units")
+
+#Additional
+print("The perimeter of circle with radius ",r," is ",(2*3.14159265358979323846*r)," units")
