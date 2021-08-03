2d8b1cf4cd6acb7e32c795e96ceb3c8776b75f75
AcnoAamir <54440357+AcnoAamir@users.noreply.github.com>
2021-08-03 21:26:39 +0530

    Added case if input is incorrect
    Added the while loop incase input is incorrect

--- MyCap_Python/area.py
+++ MyCap_Python/area.py
@@ -6,7 +6,11 @@
 r=float(input("Enter the radius of the circle to find area for "))
 # pi=3.14159265358979323846
 # area=pi*r*r
+while(r<=0):
+    print("Error, radius is incorrect please try again")
+    r=float(input("Enter the radius of the circle to find area for "))
+
 print("The area of circle with radius ",r," is ",(3.14159265358979323846*r*r)," sq units")
 
-#Additional
+#Additional, perimeter of the circle
 print("The perimeter of circle with radius ",r," is ",(2*3.14159265358979323846*r)," units")
